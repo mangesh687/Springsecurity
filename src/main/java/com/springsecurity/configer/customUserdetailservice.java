a5ed63100052197bbb5c4b467d35bb6c6b48089a
Mangesh Pakhale <mangeshpakhale2016@gmail.com>
2024-05-11 12:15:56 +0530

    db to add

---
+++ src/main/java/com/springsecurity/configer/customUserdetailservice.java
@@ -0,0 +1,15 @@
+package com.springsecurity.configer;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class customUserdetailservice implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
